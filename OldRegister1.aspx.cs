6f2c0fd99f94023bb1f3abf1ea0fc806e08fb6d8
Nishit Tated <nishittated@gmail.com>
2018-01-25 13:14:12 +0530

    Update OldRegister1.aspx.cs

--- OldRegister1.aspx.cs
+++ OldRegister1.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
-using System.Data;
+using System.Data; 
 
 public partial class OldRegister1 : System.Web.UI.Page
 {
