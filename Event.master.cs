d0d4c89c8000aa35392fff499ae4fd516a32ad09
Nishit Tated <nishittated@gmail.com>
2018-01-25 13:12:08 +0530

    Update Event.master.cs

--- Event.master.cs
+++ Event.master.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 
 public partial class Event : System.Web.UI.MasterPage
-{
+{ 
     protected void Page_Load(object sender, EventArgs e)
     {
 
