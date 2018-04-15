3d559cc2cdf60581d5a2178829674c48e8a89e1b
Nishit Tated <nishittated@gmail.com>
2018-04-15 12:54:41 +0530

    Update OldReg.aspx.cs

--- OldReg.aspx.cs
+++ OldReg.aspx.cs
@@ -2,7 +2,7 @@
   // project name: College Alumni System
   //This project is licensed under the MIT license, see LICENSE
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic; 
 using System.Linq;
 using System.Web;
 using System.Web.UI;
