351f534cfde95245472d1eb680cdd6a357d58d61
Nishit Tated <nishittated@gmail.com>
2018-04-15 12:55:38 +0530

    Update AlumniHome.aspx.cs

--- AlumniHome.aspx.cs
+++ AlumniHome.aspx.cs
@@ -1,7 +1,7 @@
 //Copyright (c) Nishit Tated. All Rights Reserved.
   // project name: College Alumni System
   //This project is licensed under the MIT license, see LICENSE
-using System;
+using System; 
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
