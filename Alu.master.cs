8f420b991342de79ea67ccf4be01fc5440f5a940
Nishit Tated <nishittated@gmail.com>
2018-04-24 16:30:26 +0530

    Update Alu.master.cs

--- Alu.master.cs
+++ Alu.master.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+ 
 public partial class Alu : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
