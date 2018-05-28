d3f347b9b5992113f7f50b8400d4ff01cd5f9def
Nishit Tated <nishittated@gmail.com>
2018-05-29 00:00:22 +0530

    Update OldRegister1.aspx.cs

--- OldRegister1.aspx.cs
+++ OldRegister1.aspx.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq; 
 using System.Web;
-using System.Web.UI;
+using System.Web.UI; 
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data; 
