8c5dc2ece348a0f93da0a01541abf17dab5fefb7
Nishit Tated <nishittated@gmail.com>
2018-07-03 20:13:13 +0530

    Update AlumniHome.aspx.cs

--- AlumniHome.aspx.cs
+++ AlumniHome.aspx.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Web.UI.WebControls; 
 
 public partial class AlumniHome : System.Web.UI.Page
 {
