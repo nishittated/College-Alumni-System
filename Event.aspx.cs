b2bce98d2cc225c585c480401a41e4c7bc520114
Nishit Tated <nishittated@gmail.com>
2018-07-03 20:12:53 +0530

    Update Event.aspx.cs

--- Event.aspx.cs
+++ Event.aspx.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data.SqlClient;
+using System.Data.SqlClient; 
 using System.Data;
 using System.Configuration;
 using System.Web.Security;
