c372eafa8f26ca837bf6dfeff6fa442678898c7c
Nishit Tated <nishittated@gmail.com>
2018-07-03 20:13:52 +0530

    Update Alu.master.cs

--- Alu.master.cs
+++ Alu.master.cs
@@ -3,7 +3,7 @@
   //This project is licensed under the MIT license, see LICENSE.--%>
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; 
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
